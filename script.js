ad7cd77d1b9e87e26ecbb21a89134b0da3332660
keshawmaitry <maitrykeshaw@gmail.com>
2025-04-28 13:42:44 +0530

    fdfsd

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-window.addEventListener('load', () => {
-  const originalTitle = document.title;
-  const attentionTitle = '*Come Back!';
+window.onload = function() {
 
-  let favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
-  favicon.rel = 'icon';
-  if (!favicon.href) {
-      favicon.href = '/favicon.ico'; // Default favicon if none exists
-      document.head.appendChild(favicon);
-  }
-  const originalFaviconHref = favicon.href;
-  const attentionFaviconHref = 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Red_circle.svg';
-
-  let blinkInterval = null;
-  let isBlinking = false;
-  let toggleState = false;
-
-  document.addEventListener('visibilitychange', () => {
-      if (document.hidden && !isBlinking) {
-          startBlinking();
-      } else {
-          stopBlinking();
+    var pageTitle = document.title;
+    var attentionMessage = '*Come Back!';
+    var blinkEvent = null;
+  
+    document.addEventListener('visibilitychange', function(e) {
+      var isPageActive = !document.hidden;
+  
+      if(!isPageActive){
+        blink();
+      }else {
+        document.title = pageTitle;
+        clearInterval(blinkEvent);
       }
-  });
-
-  function startBlinking() {
-      isBlinking = true;
-      blinkInterval = setInterval(() => {
-          toggleState = !toggleState;
-          document.title = toggleState ? attentionTitle : originalTitle;
-          favicon.href = toggleState ? attentionFaviconHref : originalFaviconHref;
-      }, 500);
-  }
-
-  function stopBlinking() {
-      isBlinking = false;
-      clearInterval(blinkInterval);
-      document.title = originalTitle;
-      favicon.href = originalFaviconHref;
-  }
-});
+    });
+  
+    function blink(){
+      blinkEvent = setInterval(function() {
+        if(document.title === attentionMessage){
+          document.title = pageTitle;
+        }else {
+          document.title = attentionMessage;
+        }
+      }, 100);
+    }
+  };
+  
