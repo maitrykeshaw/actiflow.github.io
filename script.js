d0bc2e0a8d8df7aead3f3d7bb1c0e8d860514b9a
keshawmaitry <maitrykeshaw@gmail.com>
2025-04-28 13:31:22 +0530

    efdf

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-window.onload = function() {
+window.addEventListener('load', () => {
+  const originalTitle = document.title;
+  const attentionTitle = '*Come Back!';
+  const originalFavicon = document.querySelector("link[rel='icon']");
+  const faviconHref = originalFavicon ? originalFavicon.href : '';
+  
+  const attentionFaviconURL = 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Red_circle.svg'; // 🔴 Red dot emoji favicon
+  
+  let blinkInterval = null;
+  let isBlinking = false;
+  let toggleState = false;
 
-    var pageTitle = document.title;
-    var attentionMessage = '*Come Back!';
-    var blinkEvent = null;
-  
-    document.addEventListener('visibilitychange', function(e) {
-      var isPageActive = !document.hidden;
-  
-      if(!isPageActive){
-        blink();
-      }else {
-        document.title = pageTitle;
-        clearInterval(blinkEvent);
+  document.addEventListener('visibilitychange', () => {
+      if (document.hidden && !isBlinking) {
+          startBlinking();
+      } else {
+          stopBlinking();
       }
-    });
-  
-    function blink(){
-      blinkEvent = setInterval(function() {
-        if(document.title === attentionMessage){
-          document.title = pageTitle;
-        }else {
-          document.title = attentionMessage;
-        }
-      }, 100);
-    }
-  };
-  
+  });
+
+  function startBlinking() {
+      isBlinking = true;
+      blinkInterval = setInterval(() => {
+          toggleState = !toggleState;
+          document.title = toggleState ? attentionTitle : originalTitle;
+          updateFavicon(toggleState ? attentionFaviconURL : faviconHref);
+      }, 500); // 500ms interval
+  }
+
+  function stopBlinking() {
+      isBlinking = false;
+      clearInterval(blinkInterval);
+      document.title = originalTitle;
+      updateFavicon(faviconHref);
+  }
+
+  function updateFavicon(url) {
+      if (!url) return;
+      let favicon = document.querySelector("link[rel='icon']");
+      if (favicon) {
+          favicon.href = url;
+      } else {
+          favicon = document.createElement('link');
+          favicon.rel = 'icon';
+          favicon.href = url;
+          document.head.appendChild(favicon);
+      }
+  }
+});
